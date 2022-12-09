ffa55bb9091c3c0fa48a0345aaea186a784ea475
aniket-vasy <aniketsen.vasyerp@gmail.com>
2022-12-09 19:04:40 +0530

    demo created

---
+++ src/paytm-button/paytmButton.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from "react"
+const PaytmChecksum = require('./paytmChecksum');
+const https = require('https');
+
+export function PaytmButton () {
+
+    const [paymentData, setPaymentData] = useState({
+        token: "", 
+        order: "", 
+        mid: "",
+        amount: ""
+    });
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        initialize();
+    }, []);
+
+    const initialize = () => {
+        let orderId = 'Order_'+new Date().getTime();
+
+        // Sandbox Credentials
+        let mid = "AqSipo92499010904391"; // Merchant ID
+        let mkey = "nCVjURN@UrBq9mnX"; // Merhcant Key
+        var paytmParams = {};
+
+        paytmParams.body = {
+          "requestType"  : "Payment",
+          "mid"      : mid,
+          "websiteName"  : "WEBSTAGING",
+          "orderId"    : orderId,
+          "callbackUrl"  : "http://localhost:3000/callback",
+          "txnAmount"   : {
+            "value"   : 100,
+            "currency" : "INR",
+          },
+          "userInfo"   : {
+            "custId"  : '1001',
+          }
+        };
+
+        PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), mkey).then(function(checksum){
+            console.log("checksum",checksum);
+          paytmParams.head = {
+            "signature" : checksum
+          };
+
+          var post_data = JSON.stringify(paytmParams);
+
+
+          console.log("paytmParams.body",paytmParams.body)
+
+          var options = {
+            /* for Staging */
+            hostname: 'securegw-stage.paytm.in',
+
+            /* for Production */
+                // hostname: 'securegw.paytm.in',
+
+            port: 443,
+            path: `/theia/api/v1/initiateTransaction?mid=${mid}&orderId=${orderId}`,
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Content-Length': post_data.length
+            }
+          };
+
+          var response = "";
+          var post_req = https.request(options, function(post_res) {
+            console.log("post_res",post_res)
+            post_res.on('data', function (chunk) {
+              response += chunk;
+            });
+                post_res.on('end', function(){
+
+              console.log('Response: ', response);
+              console.log('post data: ', post_data);
+
+                    // res.json({data: JSON.parse(response), orderId: orderId, mid: mid, amount: amount});
+                    setPaymentData({
+                        ...paymentData,
+                        token: JSON.parse(response).body.txnToken,
+                        order: orderId,
+                        mid: mid,
+                        amount: 100
+                    })
+            });
+          });
+
+          post_req.write(post_data);
+          post_req.end();
+        });
+    }
+
+    const makePayment = () => {
+        setLoading(true);
+        var config = {
+            "root":"",
+            "style": {
+              "bodyBackgroundColor": "#fafafb",
+              "bodyColor": "",
+              "themeBackgroundColor": "#0FB8C9",
+              "themeColor": "#ffffff",
+              "headerBackgroundColor": "#284055",
+              "headerColor": "#ffffff",
+              "errorColor": "",
+              "successColor": "",
+              "card": {
+                "padding": "",
+                "backgroundColor": ""
+              }
+            },
+            "data": {
+              "orderId": paymentData.order,
+              "token": paymentData.token,
+              "tokenType": "TXN_TOKEN",
+              "amount": paymentData.amount /* update amount */
+            },
+            "payMode": {
+              "labels": {},
+              "filter": {
+                "exclude": []
+              },
+              "order": [
+                  "CC",
+                  "DC",
+                  "NB",
+                  "UPI",
+                  "PPBL",
+                  "PPI",
+                  "BALANCE"
+              ]
+            },
+            "website": "WEBSTAGING",
+            "flow": "DEFAULT",
+            "merchant": {
+              "mid": paymentData.mid,
+              "redirect": false
+            },
+            "handler": {
+              "transactionStatus":function transactionStatus(paymentStatus){
+                console.log("paymentStatus => ",paymentStatus);
+                setLoading(false);
+              },
+              "notifyMerchant":function notifyMerchant(eventName,data){
+                console.log("Closed");
+                setLoading(false);
+              }
+            }
+        };
+      
+        if (window.Paytm && window.Paytm.CheckoutJS) {
+        // initialze configuration using init method
+        window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+            console.log('Before JS Checkout invoke');
+            // after successfully update configuration invoke checkoutjs
+            window.Paytm.CheckoutJS.invoke();
+        }).catch(function onError(error) {
+            console.log("config====>",config)
+            console.log("Error => ", error);
+        });
+        }
+    }
+
+    return (
+        <div>
+            {
+                loading ? (
+                    <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" />
+                ) : (
+                    <button onClick={makePayment}>Pay Now</button>
+                )
+            }
+        </div>
+    )
+}
